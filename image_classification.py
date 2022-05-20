e18dd63ef8ec88a7d1c210a3078458d302187654
Kamel Mohamed <kamel160711@gmail.com>
2022-05-20 23:22:43 +0200

    Updating image C Model

--- image_classification.py
+++ image_classification.py
@@ -5,15 +5,15 @@ from keras.applications.vgg16 import preprocess_input
 
 class ImagaeClassification:
     def __init__(self):
-        self.model = load_model('image_classification')
+        self.model = load_model('image_classification.h5')
         
     def predict(self, path):
-        my_image = load_img(path, target_size=(224, 224))
+        my_image = load_img(path, target_size=(224, 224), grayscale=True)
 
         #preprocess the image
         my_image = img_to_array(my_image)
-        my_image = my_image.reshape((1, my_image.shape[0], my_image.shape[1], my_image.shape[2]))
-        my_image = preprocess_input(my_image)
+        my_image = my_image.reshape(1, my_image.shape[0], my_image.shape[1], my_image.shape[2])
+        # my_image = preprocess_input(my_image)
 
         #make the prediction
         prediction = self.model.predict(my_image)
